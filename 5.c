125b307343cda8fbe05808f911caab1c9d6f29a8
mdwaseembnc <127919886+mdwaseembnc@users.noreply.github.com>
2024-12-31 16:39:05 +0530

    Update 5.c

--- 5.c
+++ 5.c
@@ -1,48 +1,36 @@
-// Include necessary libraries
-#include <DHT.h>
-
-// Define the pin and type of DHT sensor
-#define DHTPIN 2       // DHT11 sensor connected to digital pin 2
-#define DHTTYPE DHT11  // DHT11 sensor type
-
-// Initialize the DHT sensor
-DHT dht(DHTPIN, DHTTYPE);
-
 void setup() {
-  // Start serial communication for output
-  Serial.begin(9600);
-  Serial.println("DHT11 Sensor Test");
 
-  // Start the DHT sensor
-  dht.begin();
+pinMode (12, OUTPUT);
+
+digitalWrite(12, Low);
+
+Serial.begin (9600);
+
 }
 
 void loop() {
-  // Wait a few seconds between measurements
-  delay(2000);
 
-  // Read humidity
-  float humidity = dht.readHumidity();
-  
-  // Read temperature in Celsius
-  float temperatureC = dht.readTemperature();
+if (Serial available() >0){
 
-  // Read temperature in Fahrenheit
-  float temperatureF = dht.readTemperature(true);
+char letter = Serial.read();
 
-  // Check if any reads failed and exit loop if so
-  if (isnan(humidity) || isnan(temperatureC) || isnan(temperatureF)) {
-    Serial.println("Failed to read from DHT sensor!");
-    return;
-  }
+if (letter == '1') {
 
-  // Print the results to the Serial Monitor
-  Serial.print("Humidity: ");
-  Serial.print(humidity);
-  Serial.print(" %\t");
-  Serial.print("Temperature: ");
-  Serial.print(temperatureC);
-  Serial.print(" °C ");
-  Serial.print(temperatureF);
-  Serial.println(" °F");
+digitalWrite (12, HIGH);
+
+Serial.println("The LED ON");
+
+}
+}
+}
+else if (letter == '0') {
+
+digitalWrite (12, LOW);
+
+Serial.println("The LED of");
+
+else {
+  Serial.println("Invalid Input");
+}
+delay (200) 
 }
