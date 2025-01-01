94d2a2d406b8b716b5dc8ed6ad1f0c2e715bfc0b
mdwaseembnc <127919886+mdwaseembnc@users.noreply.github.com>
2025-01-01 19:46:23 +0530

    Update 7.c

--- 7.c
+++ 7.c
@@ -10,6 +10,66 @@ delay(1000);
 myStepper.step(-2048);
 delay(1000);
 }
+
+//2nd code without libray!!!!
+
+int motorPin1 = 8;
+int motorPin2 = 9;
+int motorPin3 = 10;
+int motorPin4 = 11;
+
+int stepSequence[4][4] = {
+  {1, 0, 0, 0},
+  {0, 1, 0, 0},
+  {0, 0, 1, 0},
+  {0, 0, 0, 1}
+};
+
+int stepDelay = 2;
+int stepIndex = 0;
+
+void setup() {
+  pinMode(motorPin1, OUTPUT);
+  pinMode(motorPin2, OUTPUT);
+  pinMode(motorPin3, OUTPUT);
+  pinMode(motorPin4, OUTPUT);
+}
+
+void loop() {
+  for (int i = 0; i < 100; i++) {
+    stepMotor(1);
+    delay(stepDelay);
+  }
+  
+  delay(1000);
+
+  for (int i = 0; i < 100; i++) {
+    stepMotor(-1);
+    delay(stepDelay);
+  }
+  
+  delay(1000);
+}
+
+void stepMotor(int direction) {
+  if (direction == 1) {
+    stepIndex++;
+    if (stepIndex >= 4) {
+      stepIndex = 0;
+    }
+  } else if (direction == -1) {
+    stepIndex--;
+    if (stepIndex < 0) {
+      stepIndex = 3;
+    }
+  }
+
+  digitalWrite(motorPin1, stepSequence[stepIndex][0]);
+  digitalWrite(motorPin2, stepSequence[stepIndex][1]);
+  digitalWrite(motorPin3, stepSequence[stepIndex][2]);
+  digitalWrite(motorPin4, stepSequence[stepIndex][3]);
+}
+
 .............................................................
 //dc motor
 void setup(){
