545fa994ae47e6a0e25df0781f39e534e7f4abf8
mdwaseembnc <127919886+mdwaseembnc@users.noreply.github.com>
2025-01-01 15:27:12 +0530

    Create 8b.c

---
+++ 8b.c
@@ -0,0 +1,27 @@
+int trigPin = 9;
+int echoPin = 8;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+}
+
+void loop() {
+  long duration, distance;
+
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  duration = pulseIn(echoPin, HIGH);
+  distance = (duration / 2) / 29.1;
+
+  Serial.print("Distance: ");
+  Serial.print(distance);
+  Serial.println(" cm");
+
+  delay(100);
+}
