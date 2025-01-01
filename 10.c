adbb98422281555c6c93bede2480ad9d43daf726
mdwaseembnc <127919886+mdwaseembnc@users.noreply.github.com>
2025-01-01 17:48:25 +0530

    Update 10.c

--- 10.c
+++ 10.c
@@ -41,3 +41,42 @@ void loop() {
     gsmSerial.write(Serial.read());
   }
 }
+
+//2nd code.........
+#include <SoftwareSerial.h>
+
+SoftwareSerial mySerial(9, 10);
+
+void setup()
+{
+  mySerial.begin(9600);  
+  Serial.begin(9600);    
+  delay(100);
+}
+
+void loop()
+{
+  if (Serial.available() > 0)
+    switch (Serial.read())
+    {
+      case 's':
+        mySerial.println("AT+CMGF=1");
+        delay(1000);
+        mySerial.println("AT+CMGS=\"+91xxxxxxxxxx\"\r");
+        delay(1000);
+        mySerial.println("Technolab creation");
+        delay(100);
+        mySerial.println((char)26);
+        delay(1000);
+        break;
+
+      case 'r':
+        mySerial.println("AT+CNMI=2,2,0,0,0");
+        delay(1000);
+        break;
+    }
+
+  if (mySerial.available() > 0)
+    Serial.write(mySerial.read());
+}
+
